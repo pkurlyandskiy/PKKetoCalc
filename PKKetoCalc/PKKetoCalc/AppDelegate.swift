23754802dd51a7509749cb46ca112e0889f84325
Pavlo Kurliandskyi <pavlo.kurliandskyi@playtech.com>
2017-01-16 22:43:00 +0200

    added blank project

---
+++ PKKetoCalc/PKKetoCalc/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  PKKetoCalc
+//
+//  Created by Pavlo Kurliandskyi on 1/16/17.
+//  Copyright © 2017 Home. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+	func applicationDidFinishLaunching(_ aNotification: Notification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(_ aNotification: Notification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
---
+++ PKKetoCalc/PKKetoCalc/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  PKKetoCalc
+//
+//  Created by Pavlo Kurliandskyi on 1/16/17.
+//  Copyright © 2017 Home. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: Any? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+
